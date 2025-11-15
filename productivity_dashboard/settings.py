84d45b5e10fa3c15b13b795f69e526af8cb0b6cb
Juan Meyer <juanmeyer@Juans-MacBook-Pro.local>
2025-11-15 15:44:09 -0600

    Add ALLOWED_HOSTS for local dev and PythonAnywhere

--- productivity_dashboard/settings.py
+++ productivity_dashboard/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-k8)_@4piy8)*(7+ni6!4s7d^h_v0xkm&(m7vjz4t)pnhbrl7e@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost', 
+    'juanmeyer.pythonanywhere.com',
+]
 
 # Application definition
 
